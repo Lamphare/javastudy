4aac93a630a7f11a4edd29aa96c5ec77004b364c
Lamphare <apple132000@hotmail.com>
2015-01-23 11:51:11 -0800

    Update CodeChallengeTriangle.java

--- CodeChallengeTriangle.java
+++ CodeChallengeTriangle.java
@@ -9,32 +9,26 @@ import java.util.Scanner;	// Scanner is an java utility that scans for user keyb
 public class CodeChallengeTriangle {
 
 	public static void main(String[] args){
-	double a;
-	double b;
-	double c;	//Three sides
 	
 	Scanner stdin = new Scanner( System.in ); //Open the resource for later input.
 	
 	System.out.println("Give me the first side for your triangle.");
-	a = stdin.nextDouble();		//It only accept double as input. stdin.next[type] is the syntax. for text, use [Line]
+	double a = stdin.nextDouble();		//It only accept double as input. stdin.next[type] is the syntax. for text, use [Line]
 	System.out.println("Give me the second side for your triangle.");		//currently anything else 
-	b = stdin.nextDouble();		//e.g. characters would screw the program up.
+	double b = stdin.nextDouble();		//e.g. characters would screw the program up.
 	System.out.println("Give me the last side for your triangle.");
-	c = stdin.nextDouble();
-	}
+	double c = stdin.nextDouble();
 	
-	public static double TrianglularPSA( double a, double b, double c ){ //Modularization. including perimeter area semiperimeter
 	double p = (a + b + c);		//perimeter
 	double s = (a + b + c)/2;	//semi-perimeter
-	double A = Math.sqrt ( s * ( s - a ) * ( s - b ) * ( s - c));	//Area
+	double X = Math.sqrt ( s * ( s - a ) * ( s - b ) * ( s - c));	//Area
 	
-	}
 	
-	public static void printout(String[] args){
-	System.out.println("The Perimeter of your triangle is " + p + ", and the area is" + a);
+	System.out.println("The Perimeter of your triangle is " + p + ", and the area is" + X);
+	
 	}
 	/**	this should look fine up to this point.
-	*inserting module after main and before printout.
+	*inserting module after asking sides and before printout.
 	*Then add corresponding print for new module.
 	*/
 }
