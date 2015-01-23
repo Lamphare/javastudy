5b28f4c9cf714bf4e68d41c5c7e399eb41d14833
John Reagan Stovall <reaganstovall@gmail.com>
2015-01-23 15:45:06 -0800

    Update CodeChallengeTriangle.java

--- CodeChallengeTriangle.java
+++ CodeChallengeTriangle.java
@@ -6,7 +6,7 @@
 
 import java.util.Scanner;	// Scanner is an java utility that scans for user keyboard input.
 
-public class CodeChallengeTriangle {
+public class CodeChallengeTriangle1 {
 
 	public static void main(String[] args){
 	
@@ -23,8 +23,17 @@ public class CodeChallengeTriangle {
 	double s = (a + b + c)/2;	//semi-perimeter
 	double X = Math.sqrt ( s * ( s - a ) * ( s - b ) * ( s - c));	//Area
 	
+	double angleA = Math.acos(( b * b + c * c - a * a ) / (2.0 * b * c )) * (180.0 / Math.PI);	
+	double angleB = Math.acos(( a * a + c * c - b * b ) / (2.0 * a * c )) * (180.0 / Math.PI); 	
+	double angleC = Math.acos(( a * a + b * b - c * c ) / (2.0 * a * b )) * (180.0 / Math.PI); 
 	
-	System.out.println("The Perimeter of your triangle is " + p + ", and the area is" + X);
+	System.out.println();
+	System.out.printf("The Perimeter of your triangle is  %1.2f, and the area is %1.2f",p ,X );
+	System.out.println();
+	System.out.println();
+	System.out.println("The interior angles oposite and respective to the sides a, b, and c, are ");
+	System.out.println();
+	System.out.printf( " %1.2f°, %1.2f°, %1.2f°", angleA , angleB, angleC );
 	
 	}
 	/**	this should look fine up to this point.
@@ -32,4 +41,3 @@ public class CodeChallengeTriangle {
 	*Then add corresponding print for new module.
 	*/
 }
-
